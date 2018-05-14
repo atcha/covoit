cfd16874711e478c839edc831a9455983898d87e
atcha <francois.cornuaud@gmail.com>
2018-05-14 18:09:43 +0200

    ADD - Page profile

--- web/src/pages/Profile.vue
+++ web/src/pages/Profile.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <h3 class="text-secondary">Laurent BRUN</h3>
+    <div class="avatar-container">
+      <img src="../assets/avatar.jpeg" height="140" width="140"/></div>
   </q-page>
 </template>
 
