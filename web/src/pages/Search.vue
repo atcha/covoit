421dc773697685cb9749ff8c7c16b7b7af1846bf
atcha <francois.cornuaud@gmail.com>
2018-05-16 23:43:45 +0200

    UPD - Starting modifying covoit search

--- web/src/pages/Search.vue
+++ web/src/pages/Search.vue
@@ -5,29 +5,18 @@
       <q-list-header>Votre recherche</q-list-header>
       <q-item-separator/>
       <q-item class="search-form">
-        <div tabindex="-1" class="q-if row no-wrap items-end relative-position q-input q-search text-primary">
-          <i aria-hidden="true" class="q-icon q-if-control q-if-control-before material-icons">search</i>
-          <div class="q-if-inner col row no-wrap relative-position">
-            <GmapAutocomplete placeholder="Votre adresse" @place_changed="setPlace" class="col q-input-target q-no-input-spinner text-left"></GmapAutocomplete>
-          </div>
-        </div>
-        <q-btn dense outline color="primary" label="Ajouter" @click="usePlace"/>
+        <q-input
+          float-label="Votre lieu de départ"
+          class="full-width"
+          :before="[
+            {
+              icon: 'search'
+            }
+          ]"
+        />
       </q-item>
       <q-item>
-        <GmapMap style="width: 100%; height: 70vh;" :zoom="10" :center="{lat: 46.5833, lng: 0.3333}">
-        <GmapMarker v-for="(marker, index) in markers"
-                    :key="index"
-                    :position="marker.position"
-        />
-        <GmapMarker
-          v-if="this.place"
-          label="★"
-          :position="{
-          lat: this.place.geometry.location.lat(),
-          lng: this.place.geometry.location.lng(),
-        }"
-        />
-      </GmapMap>
+
       </q-item>
     </q-list>
   </q-page>
