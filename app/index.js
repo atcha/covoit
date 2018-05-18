55651de5c17d162f7ef0ae4b8363ffdebee761c1
ThibaudL <lamarchethibaud@hotmail.fr>
2018-05-18 13:36:27 +0200

    Added user service

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import LOGGER from './utils/logger';
 import GeocodingService from './geocoding/geocoding.service';
+import UsersService from './users/users.service';
 import DeployDb from './utils/DeployDB';
+import bodyParser from 'body-parser';
 
 LOGGER.info("Starting server...");
 
@@ -9,10 +11,13 @@ const express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
+app.use(bodyParser.json()); // for parsing application/json
+
 
 DeployDb.init().then(() => {
     LOGGER.info("db initialized");
     GeocodingService.registerService(app);
+    UsersService.registerService(app);
 });
 
 
