dcceb59ec11798855ba89f6fff374e83fec54517
ThibaudL <lamarchethibaud@hotmail.fr>
2018-05-25 13:33:53 +0200

    ajout route de logout

--- app/login/login.service.js
+++ app/login/login.service.js
@@ -4,7 +4,7 @@ export default {
     registerService: (app) => {
         LOGGER.info("registering login service");
         app.get('/logged-in', (req, res) => {
-            LOGGER.debug(`/logged-in`,req.user);
+            LOGGER.debug(`/logged-in`);
             if(!req.user){
                 res.status(401).send("Il faut être connecté pour utiliser cette fonctionnalitée");
                 return;
@@ -13,5 +13,10 @@ export default {
             }
         });
 
+        app.get('/logout', function(req, res){
+            LOGGER.debug(`/logout`);
+            req.logout();
+            res.redirect('/');
+        });
     }
 };
