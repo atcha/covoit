eece0b18bca44bd06d56c61bf4c74ab456faf33f
atcha <francois.cornuaud@gmail.com>
2018-05-28 23:52:13 +0200

    ADD - new route for searchhour page (used to pick your hour depending on drivers) UPD - Search page with autocomplete address

---
+++ web/src/pages/search/SearchHour.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page padding class="search-hour">
+    <q-btn flat icon="arrow_back" @click="$router.go(-1)"/>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    name: "SearchHour",
+    data() {
+      return {
+        routes: [
+          {
+            departureTime: '8:00',
+            userId: 1
+          },
+          {
+            departureTime: '8:00',
+            userId: 2
+          },
+          {
+            departureTime: '8:00',
+            userId: 30
+          },
+          {
+            departureTime: '9:00',
+            userId: 32
+          },
+          {
+            departureTime: '8:15',
+            userId: 40
+          },
+          {
+            departureTime: '8:30',
+            userId: 41
+          },
+          {
+            departureTime: '8:15',
+            userId: 42
+          },
+          {
+            departureTime: '10:00',
+            userId: 50
+          },
+          {
+            departureTime: '13:00',
+            userId: 55
+          },
+          {
+            departureTime: '12:15',
+            userId: 58
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- web/src/router/routes.js
+++ web/src/router/routes.js
@@ -11,9 +11,13 @@ export default [
       },
       {
         path: '/rechercher',
-        component: () => import('pages/Search')
+        component: () => import('pages/search/Search')
       },
-
+      {
+        name: 'searchhour',
+        path: '/rechercher-horaire',
+        component: () => import('pages/search/SearchHour')
+      },
       {
         path: '/proposer',
         component: () => import('pages/trip/Trip'),
